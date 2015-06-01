f645a04aba43983ed9a3d27db0d74c36143bb9ec
nikolalsvk <nikoladjuza@live.com>
2015-06-01 19:02:06 +0200

    Improved security

--- all/lib/all.rb
+++
@@ -1,5 +0,0 @@
-require "all/version"
-
-module All
-  # Your code goes here...
-end
--- app/controllers/char_attributes_controller.rb
+++ app/controllers/char_attributes_controller.rb
@@ -1,21 +1,23 @@
 class CharAttributesController < ApplicationController	
 	respond_to :html, :js
-
+	
+	before_action :find_char_attribute, only: [:show, :edit, :destroy, :update]
+	
 	def show
-		@char_attribute = CharAttribute.find(params[:id])
+		
 	end
 
 	def new
-		@char_type = CharType.find(params[:char_type_id])
+		@char_type = current_user.char_types.find(params[:char_type_id])
 		@char_attribute = @char_type.char_attributes.new
 	end
 
 	def edit
-		@char_attribute = CharAttribute.find(params[:id])
+		
 	end
 
 	def create
-		@char_type = CharType.find(params[:char_type_id])
+		@char_type = current_user.char_types.find(params[:char_type_id])
 		@char_attribute = @char_type.char_attributes.create(char_attributes_params)
 		
 		if @char_attribute.save
@@ -26,19 +28,14 @@ class CharAttributesController < ApplicationController
 	end
 	
 	def update
-		@char_attribute = CharAttribute.find(params[:id])
-		@char_type = CharType.find(@char_attribute.char_type_id)
-		
 		if @char_attribute.update(char_attributes_params)
-			redirect_to char_type_path(@char_type)
+			redirect_to char_type_path(@char_attribute.char_type)
 		else
 			render 'edit'
 		end
 	end
 	
 	def destroy
-		@char_attribute = CharAttribute.find(params[:id])
-		@char_type = CharType.find(@char_attribute.char_type_id)
 		@char_attribute.destroy
 		
 		redirect_to users_char_types_path
@@ -49,7 +46,15 @@ class CharAttributesController < ApplicationController
 			params.require(:char_attribute).permit(:title, :value, :icon)
 		end
 		
+		# improved security with this function being called before some actions
 		def find_char_attribute
 			@char_attribute = CharAttribute.find(params[:id])
+			
+			if current_user.char_types.find_by_id(@char_attribute.char_type_id).present?
+				@char_attribute = current_user.char_types.find_by_id(@char_attribute.char_type_id).char_attributes.find(params[:id])
+			else
+				redirect_to users_char_types_path
+			end
+			
 		end
 end
