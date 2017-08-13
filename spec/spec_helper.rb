a1ccae68b152ec8f35c9ddad62fa0a740f0cde0f
nikolalsvk <nikolaseap@gmail.com>
2017-08-13 11:06:37 +0000

    Run simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -26,14 +29,14 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-	
+
 	# Devise test helper
 	config.include Devise::TestHelpers, type: :controller
 	config.include Capybara::DSL
-	
+
 	# Picture upload test helper
 	config.include ActionDispatch::TestProcess
-	
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
